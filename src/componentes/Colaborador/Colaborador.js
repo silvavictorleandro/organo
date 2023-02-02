d0101e73f1f098a4ed3bbf23a5dce556d472763a
Victor Silva <silvavictorleandro@outlook.com>
2023-02-02 07:40:46 -0300

    Alteração no select para melhor visualização dos nomes

--- src/componentes/Colaborador/Colaborador.js
+++ src/componentes/Colaborador/Colaborador.js
@@ -1,12 +1,12 @@
-export const Colaborador = (props) => {
+export const Colaborador = ({ nome, imagem, cargo, corDeFundo }) => {
   return(
     <div className="colaborador">
-      <div className="cabecalho">
-        <img src={props.imagem} alt='Imagem do colaborador' />
+      <div className="cabecalho" style={{ backgroundColor: corDeFundo }}>
+        <img src={imagem} alt='Imagem do colaborador' />
       </div>
       <div className="rodape">
-        <h4>{props.nome}</h4>
-        <h5>{props.cargo}</h5>
+        <h4>{nome}</h4>
+        <h5>{cargo}</h5>
       </div>
     </div>
   )
--- src/componentes/ListaSuspensa/ListaSuspensa.js
+++ src/componentes/ListaSuspensa/ListaSuspensa.js
@@ -2,7 +2,8 @@ export const ListaSuspensa = (props) => {
   return (
     <div className="lista-suspensa">
       <label>{props.label}</label>
-      <select onChange={e => props.aoAlterado(e.target.value)} required={props.obrigatorio} value={props.value}>
+      <select onChange={e => props.aoAlterado(e.target.value)} required={props.required} value={props.valor}>
+        <option value='' disabled selected>Selecione o seu time</option>
         {props.itens.map(item => <option key={item}>{item}</option>)}
       </select>
     </div>
